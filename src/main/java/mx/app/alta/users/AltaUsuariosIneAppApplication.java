d866f80d3359366a0f9101774226a56fb6198c9d
KAIROS DS Adiel Hernandez Polvo <e_ahpolvo+coppel@bancoppel.io>
2026-08-08 00:17:49 -0600

    Se sube la alta de usuarios

---
+++ src/main/java/mx/app/alta/users/AltaUsuariosIneAppApplication.java
@@ -0,0 +1,13 @@
+package mx.app.alta.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltaUsuariosIneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltaUsuariosIneAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/app/alta/users/AltaUsuariosIneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.app.alta.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltaUsuariosIneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
